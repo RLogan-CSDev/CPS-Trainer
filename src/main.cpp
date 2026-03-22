8bf523ac33a4d7fcd092fa37e361a10f3c067fa2
Robert Logan <relogan02@outlook.com>
2026-03-22 12:21:01 -0400

    Update: Created a file structure and properly linked my IDE with the SFML 3.0 library.

--- src/main.cpp
+++ src/main.cpp
@@ -3,22 +3,5 @@
 using namespace sf;
 
 int main() {
-	RenderWindow window(VideoMode({ 800,600 }), "CPS Trainer - Test");
-
-	CircleShape shape(50.f);
-	shape.setFillColor(Color::Green);
-	shape.setPosition({ 375.f,275.f });
-
-	while (window.isOpen()) {
-		while (const std::optional event = window.pollEvent()) {
-			if (event->is<Event::Closed>()) {
-				window.close();
-			}
-		}
-
-		window.clear();
-		window.draw(shape);
-		window.display();
-	}
-	return 0;
+	
 }
